7c03dd81ff2b48bfb1db867e01ea837b5695996c
bej921 <bej921@gmail.com>
2021-05-04 05:54:12 +0000

    add login/out

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,7 @@
 
 get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc) 
+    @current_user = User.find_by(id: session[:user_id])
     erb(:index)
 end
 
@@ -19,11 +20,33 @@ post '/signup' do
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
     
     if @user.save
-       "User #{username} s aved!"
-
+       redirect to('/login')
     else
         erb(:signup)
     end
 
   end
 
+get '/login' do
+      erb(:login)
+  end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    user = User.find_by(username: username)
+    
+      if user && @user.authenticate(password)
+        session[:user_id] = @user.id
+        redirect to('/')
+      else
+        @error_message = "Login failed"
+        erb(:login)
+      end
+  end
+
+  get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+  end
